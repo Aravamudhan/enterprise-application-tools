8db5307b5c8bab3e98e5032afc018f0fb9cc5533
Amudhan <aravamudhan.a@gmail.com>
2016-06-18 22:57:52 +0530

    Updated formatters

--- springcore/src/main/java/com/amudhan/springcore/formatter/FormatterServiceApp.java
+++ springcore/src/main/java/com/amudhan/springcore/formatter/FormatterServiceApp.java
@@ -4,20 +4,30 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.format.support.FormattingConversionService;
 
+import com.amudhan.springcore.conversion.TopSellingBook;
+
 public class FormatterServiceApp {
 	private static ApplicationContext context;
 	private static FormattingConversionService formattingConversionService;
 	public static void main(String[] args){
-		context=new ClassPathXmlApplicationContext("conversionserviceconfig.xml");
-		formattingConversionService=(FormattingConversionService)context.getBean("formattingConversionService");
+		context=new ClassPathXmlApplicationContext("formattingserviceconfig.xml");
+		formattingConversionService=(FormattingConversionService)context.getBean("conversionService");
+		/*Explicitly calling convert method*/
 		DebitCardNumber debitCardNumber=formattingConversionService.convert("1234-3242-4533-3432",
 																			DebitCardNumber.class);
 		System.out.println(debitCardNumber);
-		/*System.out.println(formattingConversionService.canConvert(String.class, DebitCardNumber.class));*/
+		System.out.println(formattingConversionService.canConvert(String.class, DebitCardNumber.class));
 		/*for(String s:context.getBeanDefinitionNames())
 			System.out.println(s);*/
 		
-		/*CardDetails cardDetails=(CardDetails)context.getBean("cardDetails");
-		System.out.println(cardDetails.getDebitCardNumber());*/
+		/*The cardDetails bean is created in the formattingserviceconfig.xml,
+		with a DebitCardNumber bean injected into it. 
+		The DebitCardBean is injected by injecting a string into the debitCardNumber property,
+		which is formatted by using the DebitCardNumberFormatter*/
+		CardDetails cardDetails=(CardDetails)context.getBean("cardDetails");
+		System.out.println(cardDetails.getDebitCardNumber());
+		
+		TopSellingBook topSellingBook=(TopSellingBook)context.getBean("topSellingBook");
+		System.out.println(topSellingBook.getBook());
 	}
 }
